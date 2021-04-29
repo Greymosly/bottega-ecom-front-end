977e06e9818cbdd1e575eddb0f65b66ae5361e75
JohnWGreenlaw <bottegajohngreenlaw@gmail.com>
2021-04-29 16:39:45 -0600

    styled header and navbar component

--- src/components/headernavbar/navbar.js
+++ src/components/headernavbar/navbar.js
@@ -1,30 +1,32 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import { connect } from 'react-redux';
+import { connect } from "react-redux";
 
 class Navbar extends Component {
-    render() {
-        return (
-            <div className="navbar">
-               {
-                   this.props.navbarLinks.map((link, index) => {
-                       return (
-                           <a className='navbar__link' key={index} onClick={() => console.log('trying to sswitch tab')}>
-                               {link.title}
-                           </a>
-                       )
-                   })
-               } 
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="navbar">
+        {this.props.navbarLinks.map((link, index) => {
+          return (
+            <a
+              className={`navbar__link ${link.active ? "green-text" : ""}`}
+              key={index}
+              onClick={() => console.log("trying to sswitch tab")}
+            >
+              {link.title}
+            </a>
+          );
+        })}
+      </div>
+    );
+  }
 }
 
 function mapStateToProps(state) {
-    const{ navbarLinks } = state.headerNavbar;
-    return {
-        navbarLinks
-    }
+  const { navbarLinks } = state.headerNavbar;
+  return {
+    navbarLinks,
+  };
 }
 
 Navbar = connect(mapStateToProps)(Navbar);
