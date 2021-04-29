ac66b9a474f2892c1b9ef258e426886723f2cdcd
JohnWGreenlaw <bottegajohngreenlaw@gmail.com>
2021-04-29 16:51:22 -0600

    built base structure for account components

---
+++ src/components/account/purchaseHistory.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class PurhcaseHistory extends Component {
+    render() {
+        <div>
+            purhcase history 
+        </div>
+    }
+}
+
+export default PurhcaseHistory;
