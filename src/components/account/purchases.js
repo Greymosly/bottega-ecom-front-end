072d8bd11bbdd9f8aaffcec98890c5b62a809c03
JohnWGreenlaw <bottegajohngreenlaw@gmail.com>
2021-05-01 12:14:50 -0600

    implemented set purchase detail action creator and reducer

--- src/components/account/purchases.js
+++ src/components/account/purchases.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 
-import { connect } from 'react-redux';
-import * as actions from '../../actions';
+import { connect } from "react-redux";
+import * as actions from "../../actions";
 
 class Purchases extends Component {
   componentDidMount() {
@@ -11,25 +11,31 @@ class Purchases extends Component {
   render() {
     const { className } = this.props;
 
-    return <div className={`${className} purchases`}>
-        {
-            this.props.purchases.map(purchase => {
-                return (
-                    <div key={purchase._id} className='purchases__purchase purchase'>
-                        <img className='purchase__img' src='http://via.placeholder.com/80x80'/>
-                    </div>
-                )
-            })
-        }
-    </div>;
+    return (
+      <div className={`${className} purchases`}>
+        {this.props.purchases.map((purchase) => {
+          return (
+            <a
+              onClick={() => this.props.setPurchaseDetail(purchase._id)}
+              key={purchase._id}
+              className="purchases__purchase purchase"
+            >
+              <img
+                className="purchase__img"
+                src="http://via.placeholder.com/80x80"
+              />
+            </a>
+          );
+        })}
+      </div>
+    );
   }
 }
 
 function mapStateToProps(state) {
-    const { purchases } = state.user;
-    return {purchases };
+  const { purchases } = state.user;
+  return { purchases };
 }
-
 
 Purchases = connect(mapStateToProps, actions)(Purchases);
 
