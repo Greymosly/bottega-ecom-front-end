122e7b290808944b07b29482170b10789fdb468a
JohnWGreenlaw <bottegajohngreenlaw@gmail.com>
2021-05-03 19:54:25 -0600

    built account informatio from from sigin from template

--- src/components/account/accountInformation.js
+++ src/components/account/accountInformation.js
@@ -1,13 +1,27 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+
+import PageTitle from "../pageTitle";
+import AccountInformationForm from './accountInformationForm';
 
 class AccountInformation extends Component {
-    render() {
-        return (
-            <div>
-                account info
-            </div>
-        )
-    }
+  onSubmit = (fields) => {
+    console.log(fields);
+  };
+
+  render() {
+    return (
+      <div className="account-information">
+        <PageTitle
+          className="account-information"
+          title="Account Information"
+        />
+        <AccountInformationForm
+          onSubmit={this.onSubmit}
+          className="account-information__form"
+        />
+      </div>
+    );
+  }
 }
 
 export default AccountInformation;
