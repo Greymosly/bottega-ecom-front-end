2a64e21344f3173b040dcff9068c9ae21e51e716
JohnWGreenlaw <bottegajohngreenlaw@gmail.com>
2021-05-05 11:30:44 -0600

    added shop cart component to shop.js

--- src/components/shop/shop.js
+++ src/components/shop/shop.js
@@ -2,11 +2,19 @@ import React, { Component } from "react";
 
 import { connect } from "react-redux";
 import * as actions from "../../actions";
-import ShopSearchBar from './shopSearchBar';
-import ShopProduct from './shopProduct';
-import ShopCart from './shopCart';
+import ShopSearchBar from "./shopSearchBar";
+import ShopProduct from "./shopProduct";
+import ShopCart from "./shopCart";
 
 class Shop extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      showCart: true,
+    };
+  }
+
   componentDidMount() {
     const headerLinks = [
       {
@@ -32,21 +40,19 @@ class Shop extends Component {
   }
 
   onSubmit = (fields) => {
-      this.props.filterProductsWithQuery(fields)
-  }
+    this.props.filterProductsWithQuery(fields);
+  };
 
   render() {
     return (
       <div className="shop">
-        <ShopCart />
-        <ShopSearchBar onSubmit={this.onSubmit} className='shop__search-bar'/>
+        <ShopSearchBar onSubmit={this.onSubmit} className="shop__search-bar" />
         <div className="shop__products">
           {this.props.filteredProducts.map((product) => {
-            return (
-              <ShopProduct {...product} key={product._id}/>
-            );
+            return <ShopProduct {...product} key={product._id} />;
           })}
         </div>
+        {this.state.showCart ? <ShopCart className="shop__cart" /> : ""}
         {/* shop car button */}
       </div>
     );
--- src/components/shop/shopCart.js
+++ src/components/shop/shopCart.js
@@ -3,12 +3,12 @@ import React, { Component } from 'react';
 function CartButton({className, icon}) {
     return (
         <div className={`${className} cart-button`}>
-            <i className={icon} />
+            <i className={icon}/>        
         </div>
     )
 }
 
-function CartContent({ className }) {
+function CartContent({className}) {
     return (
         <div className={`${className} cart-content`}>
 
@@ -22,10 +22,9 @@ class ShopCart extends Component {
         return (
             <div className={`${className} shop-cart`}>
                 <CartButton className='shop-cart__toggle' icon='fas fa-times'/>
-                <CartContent className='shop-cart__content' />
+                <CartContent className='shop-cart__content'/>
             </div>
         )
     }
 }
-
 export default ShopCart;
