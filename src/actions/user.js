6d88845cfe5247083df1962b2aa813601d3f5643
JohnWGreenlaw <bottegajohngreenlaw@gmail.com>
2021-05-05 16:11:15 -0600

    setup cart product model and action creator to fetch and set user cart products

--- src/actions/user.js
+++ src/actions/user.js
@@ -1,107 +1,140 @@
 import {
-    SET_USER_PURCHASES,
-    SET_PURCHASE_DETAIL
-} from './types';
+  SET_USER_PURCHASES,
+  SET_PURCHASE_DETAIL,
+  SET_CART_PRODUCTS,
+} from "./types";
 
 export function setPurchaseDetail(_id) {
-    return ({
-        type: SET_PURCHASE_DETAIL,
-        payload: _id
-    })
+  return {
+    type: SET_PURCHASE_DETAIL,
+    payload: _id,
+  };
+}
+
+export function fetchCartProducts() {
+  return {
+    type: SET_CART_PRODUCTS,
+    payload: [
+      {
+        _id: 0,
+        product: {
+          _id: 0,
+          title: "JavaScript in the Browser",
+          description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident quaerat ipsam quisquam esse hic libero, atque blanditiis eum dolore corrupti consequuntur, odit beatae eius, laborum eveniet explicabo delectus tenetur totam!",
+          price: 1.99,
+          belongsTo: [0, 1],
+        },
+        quantity: 2,
+      },
+      {
+        _id: 1,
+        product: {
+          _id: 1,
+          title: "Graph Database",
+          description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident quaerat ipsam quisquam esse hic libero, atque blanditiis eum dolore corrupti consequuntur, odit beatae eius, laborum eveniet explicabo delectus tenetur totam!",
+          price: 1.99,
+          belongsTo: [0, 6],
+        },
+        quantity: 1,
+      },
+    ],
+  };
 }
 
 export function fetchUserPurchases() {
-    return ({
-        type: SET_USER_PURCHASES,
-        payload: [
-            {
-                _id: 0,
-                total: 19.40,
-                orderNumber: 'A0004828282838',
-                orderDate: new Date().toDateString(),
-                creditCard: '-0000',
-                user: {
-                    name: 'Jordan Hudgens',
-                    shippingAddress: '1234 West State Street'
-                }
-            },
-            {
-                _id: 1,
-                total: 1.30,
-                orderNumber: '3e5463463452324',
-                orderDate: new Date().toDateString(),
-                creditCard: '-0000',
-                user: {
-                    name: 'Person Person',
-                    shippingAddress: '1337 West State Street'
-                }
-            },
-            {
-                _id: 2,
-                total: 2.30,
-                orderNumber: '856345fdsre',
-                orderDate: new Date().toDateString(),
-                creditCard: '-0000',
-                user: {
-                    name: 'Another Person',
-                    shippingAddress: '1400 West State Street'
-                }
-            },
-            {
-                _id: 3,
-                total: 20.34,
-                orderNumber: 'YJ63452332',
-                orderDate: new Date().toDateString(),
-                creditCard: '-0000',
-                user: {
-                    name: 'Some one Hudgens',
-                    shippingAddress: '1000 West State Street'
-                }
-            },
-            {
-                _id: 4,
-                total: 19.40,
-                orderNumber: 'NMJF677880643',
-                orderDate: new Date().toDateString(),
-                creditCard: '-0000',
-                user: {
-                    name: 'New Person',
-                    shippingAddress: '34 West State Street'
-                }
-            },
-            {
-                _id: 5,
-                total: 2.00,
-                orderNumber: 'FGHJGHK96R5T76457',
-                orderDate: new Date().toDateString(),
-                creditCard: '-0000',
-                user: {
-                    name: 'Some One Else',
-                    shippingAddress: '67 West State Street'
-                }
-            },
-            {
-                _id: 6,
-                total: 16.34,
-                orderNumber: '34643DFGHFDH',
-                orderDate: new Date().toDateString(),
-                creditCard: '-0000',
-                user: {
-                    name: 'Ashley Gridges',
-                    shippingAddress: '1200West State Street'
-                }
-            },
-            {
-                _id: 7,
-                total: 12.30,
-                orderNumber: '784566345SFBGCGVJCGVUH',
-                orderDate: new Date().toDateString(),
-                creditCard: '-0000',
-                user: {
-                    name: 'Imogene Greenlaw',
-                    shippingAddress: '89 West State Street'
-                }
-            },
-        ]
-    })
+  return {
+    type: SET_USER_PURCHASES,
+    payload: [
+      {
+        _id: 0,
+        total: 19.4,
+        orderNumber: "A0004828282838",
+        orderDate: new Date().toDateString(),
+        creditCard: "-0000",
+        user: {
+          name: "Jordan Hudgens",
+          shippingAddress: "1234 West State Street",
+        },
+      },
+      {
+        _id: 1,
+        total: 1.3,
+        orderNumber: "3e5463463452324",
+        orderDate: new Date().toDateString(),
+        creditCard: "-0000",
+        user: {
+          name: "Person Person",
+          shippingAddress: "1337 West State Street",
+        },
+      },
+      {
+        _id: 2,
+        total: 2.3,
+        orderNumber: "856345fdsre",
+        orderDate: new Date().toDateString(),
+        creditCard: "-0000",
+        user: {
+          name: "Another Person",
+          shippingAddress: "1400 West State Street",
+        },
+      },
+      {
+        _id: 3,
+        total: 20.34,
+        orderNumber: "YJ63452332",
+        orderDate: new Date().toDateString(),
+        creditCard: "-0000",
+        user: {
+          name: "Some one Hudgens",
+          shippingAddress: "1000 West State Street",
+        },
+      },
+      {
+        _id: 4,
+        total: 19.4,
+        orderNumber: "NMJF677880643",
+        orderDate: new Date().toDateString(),
+        creditCard: "-0000",
+        user: {
+          name: "New Person",
+          shippingAddress: "34 West State Street",
+        },
+      },
+      {
+        _id: 5,
+        total: 2.0,
+        orderNumber: "FGHJGHK96R5T76457",
+        orderDate: new Date().toDateString(),
+        creditCard: "-0000",
+        user: {
+          name: "Some One Else",
+          shippingAddress: "67 West State Street",
+        },
+      },
+      {
+        _id: 6,
+        total: 16.34,
+        orderNumber: "34643DFGHFDH",
+        orderDate: new Date().toDateString(),
+        creditCard: "-0000",
+        user: {
+          name: "Ashley Gridges",
+          shippingAddress: "1200West State Street",
+        },
+      },
+      {
+        _id: 7,
+        total: 12.3,
+        orderNumber: "784566345SFBGCGVJCGVUH",
+        orderDate: new Date().toDateString(),
+        creditCard: "-0000",
+        user: {
+          name: "Imogene Greenlaw",
+          shippingAddress: "89 West State Street",
+        },
+      },
+    ],
+  };
 }
--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -1,6 +1,11 @@
-import { SET_USER_PURCHASES, SET_PURCHASE_DETAIL } from "../actions/types";
+import {
+  SET_USER_PURCHASES,
+  SET_PURCHASE_DETAIL,
+  SET_CART_PRODUCTS,
+} from "../actions/types";
 
 const INITIAL_STATE = {
+  cartProducts: [],
   purchases: [],
   purchaseDetail: {
     user: {
@@ -17,6 +22,11 @@ const INITIAL_STATE = {
 
 export default function (state = INITIAL_STATE, action) {
   switch (action.type) {
+    case SET_CART_PRODUCTS:
+      return {
+        ...state,
+        cartProducts: action.payload
+      };
     case SET_USER_PURCHASES:
       return {
         ...state,
@@ -25,7 +35,7 @@ export default function (state = INITIAL_STATE, action) {
     case SET_PURCHASE_DETAIL:
       let purchaseDetail;
       state.purchases.map((purchase) => {
-        if ((purchase._id == action.payload)) {
+        if (purchase._id == action.payload) {
           purchaseDetail = purchase;
         }
       });
